092e5f7f788cd0e647661eed982413e1e812ea27
Owen Webb <owebb@umich.edu>
2020-04-10 00:29:02 -0400

    Refacotr pref list storage with static class var

--- srp.py
+++ srp.py
@@ -6,31 +6,34 @@ from dataclasses import dataclass
 @dataclass
 class Person:
     """Dataclass representation of a person in the SRP."""
-    label: str
-    roommate: str
-    prefs: list
+    prefs = {}  # static dictionary of ALL preference lists
+
+    name: str
+    mate: str
+    plist: list
+
+    def __post_init__(self):
+        """Add Person's pref list to the dict of all pref lists."""
+        Person.prefs[self.name] = self.plist
+        self.plist = Person.prefs[self.name]
 
     def pref_of(self: Person, other: Person) -> int:
         """Returns this Person's preference for another."""
-        return len(self.prefs) - self.prefs.index(other.label)
+        return len(self.plist) - self.plist.index(other.name)
 
     def has_best_match(self: Person) -> bool:
         """Is this person currently matched with their best possible partner
         according to their current preference list?"""
-        if self.roommate:
+        if self.mate:
             for person_label in self.prefs:
                 if person_label:
-                    return person_label == self.roommate
+                    return person_label == self.mate
         return False
 
 
 def srp(people):
     """Irving's Algorithm."""
-
-    # Phase 1
-    for person in people:
-        if not person.has_best_match():
-            pass
+    pass
 
 
 if __name__ == '__main__':
