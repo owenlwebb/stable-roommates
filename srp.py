d52bbaf7da526378f67777890905e77f4dcc4426
Owen Webb <owebb@umich.edu>
2020-04-11 14:57:03 -0400

    Lots of reworks and overhauls. Phase 1 finishes successfully

--- srp.py
+++ srp.py
@@ -5,12 +5,16 @@ from dataclasses import dataclass
 
 @dataclass
 class Person:
-    """Dataclass representation of a person in the SRP."""
-    all_people = {}  # static dictionary of ALL init'd People
+    """Dataclass representation of a person in the SRP.
+
+    POC = Promise of Consideration.
+    """
+    all_people = {}     # static dictionary of ALL init'd People
 
     name: str
-    mate: str
-    plist: list
+    offer_sent: str     # Person to whom an offer was sent and a POC was recvd
+    offer_held: str     # Person who gave self an offer and self sent a POC
+    plist: list         # Preference list
 
     def __post_init__(self):
         """Add Person to the dict of all People."""
@@ -18,45 +22,100 @@ class Person:
 
     def pref_of(self: Person, other: Person) -> int:
         """Returns this Person's preference for another."""
-        return len(self.plist) - self.plist.index(other.name)
+        try:
+            return len(self.plist) - self.plist.index(other.name)
+        except ValueError:
+            # return -1 when other is not found in plist. Must be that other
+            # was set to None
+            return -1
 
-    def has_best_match(self: Person) -> bool:
-        """Is this person currently matched with their best possible partner
-        according to their current preference list?"""
-        if self.mate:
-            for name in self.plist:
-                if name:
-                    return name == self.mate
-        return False
-
-    def get_next_highest(self: Person) -> str:
+    def get_next_highest(self: Person) -> Person:
         """Get next highest preferred roommate in plist."""
-        for person in self.plist:
-            if person:
-                return Person.all_people[person]
-        return None
+        return next(Person.all_people[p] for p in self.plist if p)
+
+    def remove(self: Person, other: Person) -> None:
+        """Remove other from self's preference list."""
+        self.plist[self.plist.index(other.name)] = None
+
+    def reject(self: Person, other: Person) -> None:
+        """self and other symetrically reject."""
+        assert self.offer_held and (self.offer_held == other.name)
+        assert other.offer_sent and (other.offer_sent == self.name)
+
+        other.offer_sent = None
+        self.offer_held = None
+        other.remove(self)
+        self.remove(other)
 
     @staticmethod
-    def reset():
+    def get_person(name: str) -> Person:
+        """Return the person object corresponding to name."""
+        return Person.all_people[name] if name else None
+
+    @staticmethod
+    def reset() -> None:
         """Reset the Person-wide all_people to an empty dictionary."""
         Person.all_people = {}
+
+    @staticmethod
+    def print_pref_table() -> None:
+        """Print the table of people and their preference lists."""
+        for person in sorted(Person.all_people.values(), key=lambda p: p.name):
+            print_list = [p if p else ' ' for p in person.plist]
+            print(f"{person.name} | " + ' '.join(print_list))
+
+
+def gen_offerers(people):
+    """Yield the next Person that is eligible to make an offer."""
+    all_matched = False
+    while not all_matched:
+        all_matched = True  # assume and correct if not
+        for person in people:
+            if not person.offer_sent and any(person.plist):
+                all_matched = False
+                yield person
 
 
 def srp(people):
     """Irving's Algorithm."""
 
     # PHASE 1
-    for person in (p for p in people if not p.has_best_match()):
-        potential = person.get_next_highest()
+    while True:
+        # get next offerer
+        try:
+            offerer = next(gen_offerers(people))
+        except StopIteration:
+            break
+
+        # get next offeree and the current person they've given a POC to
+        offeree = offerer.get_next_highest()
+        offeree_poc = Person.get_person(offeree.offer_held)
+
+        # Offeree accepts automatically
+        if not offeree_poc:
+            offerer.offer_sent = offeree.name
+            offeree.offer_held = offerer.name
+        # Offeree trades up
+        elif offeree.pref_of(offerer) > offeree.pref_of(offeree_poc):
+            offeree.reject(offeree_poc)  # symetrically reject former
+            offerer.offer_sent = offeree.name
+            offeree.offer_held = offerer.name
+        # Offereee rejects
+        else:
+            offerer.remove(offeree)
+            offeree.remove(offerer)
+
+    # PHASE 2 (Cycle Removal)
+    Person.print_pref_table()
 
 
 if __name__ == '__main__':
     PEOPLE = [
-        Person('A', None, ['B', 'D', 'F', 'C', 'E']),
-        Person('B', None, ['D', 'E', 'F', 'A', 'C']),
-        Person('C', None, ['D', 'E', 'F', 'A', 'B']),
-        Person('D', None, ['F', 'C', 'A', 'E', 'B']),
-        Person('E', None, ['F', 'C', 'D', 'B', 'A']),
-        Person('F', None, ['A', 'B', 'D', 'C', 'E'])
+        Person('A', None, None, ['B', 'D', 'F', 'C', 'E']),
+        Person('B', None, None, ['D', 'E', 'F', 'A', 'C']),
+        Person('C', None, None, ['D', 'E', 'F', 'A', 'B']),
+        Person('D', None, None, ['F', 'C', 'A', 'E', 'B']),
+        Person('E', None, None, ['F', 'C', 'D', 'B', 'A']),
+        Person('F', None, None, ['A', 'B', 'D', 'C', 'E'])
     ]
     srp(PEOPLE)
